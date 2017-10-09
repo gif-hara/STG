16644065d21729ce803211bdeedc4e320c4225d8
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-10-10 00:32:19 +0900

    :new: 敵弾作ってみる

---
+++ Assets/HK/STG/Scripts/DanmakuSystems/Muzzle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace HK.STG.DanmakuSystems
+{
+    public sealed class Muzzle : MonoBehaviour
+    {
+        public void Fire()
+        {
+            
+        }
+    }
+}
---
+++ Assets/HK/STG/Scripts/TextureControllers/UVAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace HK.STG.TextureControllers
+{
+    public sealed class UVAnimation : MonoBehaviour
+    {
+        void Awake()
+        {
+//            var renderer = this.GetComponent<SpriteRenderer>();
+//            this.UpdateAsObservable()
+//                .Where(_ => this.isActiveAndEnabled)
+//                .SubscribeWithState(renderer, (_, r) =>
+//                {
+//                    var material = renderer.material;
+//                })
+        }
+    }
+}
