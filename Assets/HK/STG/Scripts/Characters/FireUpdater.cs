e6034b00a5087c09cc7be3b6b95ab0c82ca9de02
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-10-14 23:40:00 +0900

    :new: FireUpdater

---
+++ Assets/HK/STG/Scripts/Characters/FireUpdater.cs
@@ -0,0 +1,23 @@
+﻿using HK.STG.Events;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace HK.STG.CharacterControllers
+{
+    [RequireComponent(typeof(Character))]
+    public sealed class FireUpdater : MonoBehaviour
+    {
+        void Awake()
+        {
+            var character = this.GetComponent<Character>();
+            this.UpdateAsObservable()
+                .Where(_ => this.isActiveAndEnabled)
+                .SubscribeWithState(character, (_, _chatacter) =>
+                {
+                    _chatacter.Broker.Publish(Fire.GetCache());
+                })
+                .AddTo(character);
+        }
+    }
+}
